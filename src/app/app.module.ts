3c040ce58ca4ee3776cbf443467034450e322701
Felipe <fliptm95@gmail.com>
2019-09-26 11:11:39 -0300

    genre and movies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CardComponent } from './pages/shared/card/card.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./core/navbar/navbar.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { CardComponent } from "./pages/shared/components/card/card.component";
+import { MoviesComponent } from './pages/movies/movies.component';
+import { GenreComponent } from './pages/genre/genre.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, CardComponent, MoviesComponent, GenreComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
